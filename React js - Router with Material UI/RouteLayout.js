008fa3ce38a4367dfb23c2e0e8b460c4f316d328
Vinoth Kumar P <vinothkumar4210@gmail.com>
2025-10-16 17:20:29 +0530

    Add RouteLayout component with Navbar and Outlet

--- React js - Router with Material UI/RouteLayout.js
+++ React js - Router with Material UI/RouteLayout.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../Components/Navbar'
 
+const RouteLayout = () => {
+  return (
+    <>
+    <Navbar />
+    <Outlet />
+    </>
+  )
+}
+
+export default RouteLayout
