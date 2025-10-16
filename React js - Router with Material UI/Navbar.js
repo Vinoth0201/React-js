5d66f997dd835ab0c4377e0b28ca94082e2abd80
Vinoth Kumar P <vinothkumar4210@gmail.com>
2025-10-16 17:09:22 +0530

    Implement Navbar component with Material UI

--- React js - Router with Material UI/Navbar.js
+++ React js - Router with Material UI/Navbar.js
@@ -1,1 +1,27 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 
+const Navbar = () => {
+  return (
+    <> 
+      <AppBar position="static">
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography variant="h6">
+            ObjectWays
+          </Typography>
+          <Box sx={{ display: 'flex', gap: '50px', alignItems: 'center' }}>
+            <Typography>Home</Typography>
+            <Typography>About</Typography>
+            <Typography>Contact</Typography>
+            <Typography>Service</Typography>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default React.memo(Navbar)
