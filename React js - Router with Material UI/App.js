a0ed6b6a9f216ab92fa5030a39288119e570ad14
Vinoth Kumar P <vinothkumar4210@gmail.com>
2025-10-16 17:10:47 +0530

    Set up routing with react-router-dom

--- React js - Router with Material UI/App.js
+++ React js - Router with Material UI/App.js
@@ -1,1 +1,36 @@
+//import logo from "./logo.svg";
+import "./App.css";
+//import StateExample from "./StateExample";
+//import ReducerExample from "./ReducerExample";
+//import ContextExample from "./ContextExample";
+//import MemoExample from "./MemoExample";
+//
+function App() {
+  return (
+    <div className="//App">
+      <MemoExample />
+    </div>
+  );
+}import Navbar from "./Components/Navbar";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import RouteLayout from "./Layouts/RouteLayout";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Service from "./Pages/Service";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RouteLayout />}>
+    <Route index element={<Home />} />
+    <Route path="About" element={<About />} />
+    <Route path="Contact" element={<Contact />} />
+    <Route path="service" element={<Service />} />
+    </Route>
+  )
+)
+
+
+export default App
 
