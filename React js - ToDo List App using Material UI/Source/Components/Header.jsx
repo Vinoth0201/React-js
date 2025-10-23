5fd9c6bf0c0f4ec096136a61bf07c9ff10c1293f
Vinoth Kumar P <vinothkumar4210@gmail.com>
2025-10-23 17:09:27 +0530

    Add Header component for ToDo List App

---
+++ React js - ToDo List App using Material UI/Source/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AnchorIcon from '@mui/icons-material/Anchor';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>
+        <AnchorIcon />
+          ToDo List App
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
